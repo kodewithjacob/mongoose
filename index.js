6f29724feaef877451b0a4693245afc4bd533d70
Jacob Fernandez <jacobdaniel.fernandez@gmail.com>
2022-09-30 18:52:32 +0800

    Add endpoints

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect(
-  `mongodb+srv://kodego:kodegopassword@kodego.xb5nkku.mongodb.net/?retryWrites=true&w=majority`
+  `mongodb+srv://user:password@kodego.xb5nkku.mongodb.net/?retryWrites=true&w=majority`
 );
 
 const Entry = mongoose.model("Entry", {
